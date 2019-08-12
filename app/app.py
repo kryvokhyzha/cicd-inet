0b6252cf2e8781081b30972a2d33d1f34d25e312
kryvokhyzha <kriwohizha@gmail.com>
2019-08-12 22:42:12 +0300

    Added gunicorn

--- app/app.py
+++ app/app.py
@@ -76,4 +76,5 @@ def detector():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.getenv('PORT'), debug=True)
+    port = int(os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0', port=5000)
